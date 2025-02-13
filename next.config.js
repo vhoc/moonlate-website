9a15144f962556c6624c56b37582ef94fe6bc2c0
Víctor Olvera <vhocar@gmail.com>
2025-02-13 15:16:32 -0600

    Adjusted path

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '', // Default to empty string if not defined
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
+};
 
 export default config;
