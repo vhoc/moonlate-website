9b8ffa13ea0cdc10eb352779a03395498f081f47
Víctor Olvera <vhocar@gmail.com>
2025-02-13 16:21:36 -0600

    Removed unused import

--- src/components/layout/HeaderBackground.tsx
+++ src/components/layout/HeaderBackground.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { usePathname } from "next/navigation"
 import Image from "next/image";
-import { cn } from "@/lib/utils"
 
 interface HeaderBackgroundProps {
   children?: React.ReactNode | React.ReactNode[]
