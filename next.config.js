569c5dc571b3c0981dde1c74020a21b82f2b2dca
Víctor Olvera <vhocar@gmail.com>
2025-02-13 15:35:59 -0600

    restored config

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '', // Default to empty string if not defined
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
 };
 
 export default config;
